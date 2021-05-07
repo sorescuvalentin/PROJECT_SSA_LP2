88e4a442a9dd724c2d05d87c2ae56cf5fe891aa9
Sorescu Valentin <70637375+sorescuvalentin@users.noreply.github.com>
2021-05-07 15:14:01 +0300

    actualizare

--- text.py
+++ text.py
@@ -2,23 +2,35 @@ from bs4 import BeautifulSoup
 import requests
 from tkinter import *
 
-urls = ['http://86.125.113.218:61978/html/timpi/trasee.php?param1=990', 'http://86.125.113.218:61978/html/timpi/trasee.php?param1=2786', 'http://86.125.113.218:61978/html/timpi/trasee.php?param1=1006', 'http://86.125.113.218:61978/html/timpi/trasee.php?param1=2766', 'http://86.125.113.218:61978/html/timpi/trasee.php?param1=989', 'http://86.125.113.218:61978/html/timpi/trasee.php?param1=1206', 'http://86.125.113.218:61978/html/timpi/trasee.php?param1=1086', 'http://86.125.113.218:61978/html/timpi/trasee.php?param1=1166']
+urls = 'http://86.125.113.218:61978/html/timpi/trol.php'
+link_de_unit = ['http://86.125.113.218:61978/html/timpi/']
+
+page = requests.get(urls)
+html = page.text
 
 class Application(Frame):
     def __init__(self, master=None, on_reload=None):
         Frame.__init__(self, master)
         self.pack()
         self.my_reload = Button(self)
-        self.my_reload["text"] = "Reload",
+        self.my_reload["text"] = "Reseteaza Traseele",
         self.my_reload["command"] = on_reload
         self.my_reload.pack()
 
+extragere_param = re.findall("(?<=')trasee.+?(?=')", html)
+lista_finala = []
+for i in link_de_unit:
+    for j in extragere_param:
+        lista_finala.append(i + j)
+
+
         with open ('dateExtrase.txt', 'w', encoding="utf-8") as f:
-            for url in urls:
+            for url in lista_finala:
                 page = requests.get(url)
                 html = page.text
                 soup = BeautifulSoup(html, 'html.parser')
                 for row in soup.find_all('tr'):
                     for col in row.find_all('td'):
                         f.write(col.text + '\n')
+
 
