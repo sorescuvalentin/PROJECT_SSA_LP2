fbb811a49aeb3935acc003e46503b3aae7e1d163
Elena3141 <82753229+Elena3141@users.noreply.github.com>
2021-05-16 21:36:28 +0300

    Add files via upload
    test

--- text.py
+++ text.py
@@ -1,36 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
-from tkinter import *
 
-urls = 'http://86.125.113.218:61978/html/timpi/trol.php'
-link_de_unit = ['http://86.125.113.218:61978/html/timpi/']
+url = "http://86.125.113.218:61978/html/timpi/trasee.php?param1=1106"
+page = requests.get(url)
+pagetext = page.text
 
-page = requests.get(urls)
-html = page.text
+pricetable = {
+    "Sosire" : [],
+}
+print(pricetable)
+soup = BeautifulSoup(pagetext, 'html.parser')
 
-class Application(Frame):
-    def __init__(self, master=None, on_reload=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.my_reload = Button(self)
-        self.my_reload["text"] = "Reseteaza Traseele",
-        self.my_reload["command"] = on_reload
-        self.my_reload.pack()
+with open ('NiftyList.txt', 'w', encoding="utf-8") as f:
 
-extragere_param = re.findall("(?<=')trasee.+?(?=')", html)
-lista_finala = []
-for i in link_de_unit:
-    for j in extragere_param:
-        lista_finala.append(i + j)
-
-
-        with open ('dateExtrase.txt', 'w', encoding="utf-8") as f:
-            for url in lista_finala:
-                page = requests.get(url)
-                html = page.text
-                soup = BeautifulSoup(html, 'html.parser')
-                for row in soup.find_all('tr'):
-                    for col in row.find_all('td'):
-                        f.write(col.text + '\n')
-
-
+    for row in soup.find_all('tr'):
+        for col in row.find_all('td'):
+            f.write(col.text + '\n')
