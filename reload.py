3d2cd058603602599511f9c16e91e637fe5bf2e6
Sorescu Valentin <70637375+sorescuvalentin@users.noreply.github.com>
2021-05-19 11:33:02 +0300

    optimizat buton de reload pentru interfata

--- reload.py
+++ reload.py
@@ -8,17 +8,12 @@ import text
 class Executor:
     def __init__(self):
         self.root = tkinter.Tk()
-        self.app = text.Application(master=self.root, on_reload=self.on_reload)
-        self.app.mainloop()
+        self.on_reload()
 
     def on_reload(self):
         self.root.destroy()
-
         importlib.reload(text)
 
-        self.root = tkinter.Tk()
-        self.app = text.Application(master=self.root, on_reload=self.on_reload)
-        self.app.mainloop()
 
 
 if __name__ == '__main__':
